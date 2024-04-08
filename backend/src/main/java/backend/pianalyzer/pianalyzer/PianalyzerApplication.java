0c7da6522c941ace8dd9995e68911d37b7254c42
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-04-08 23:22:42 +0200

    add backend structure and mongodb container

---
+++ backend/src/main/java/backend/pianalyzer/pianalyzer/PianalyzerApplication.java
@@ -0,0 +1,13 @@
+package backend.pianalyzer.pianalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PianalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PianalyzerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/backend/pianalyzer/pianalyzer/PianalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.pianalyzer.pianalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PianalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
