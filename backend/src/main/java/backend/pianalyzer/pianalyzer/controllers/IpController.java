64abf3ab7cf36a106c33cf643e12eea3417567f1
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-05-07 22:43:31 +0200

    Add IpController

---
+++ backend/src/main/java/backend/pianalyzer/pianalyzer/controllers/IpController.java
@@ -0,0 +1,24 @@
+package backend.pianalyzer.pianalyzer.controllers;
+
+import backend.pianalyzer.pianalyzer.environment.Environment;
+import backend.pianalyzer.pianalyzer.services.IpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class IpController {
+    private final IpService ipService;
+    private final String FRONTEND_URL = Environment.FRONTEND_URL;
+
+    @CrossOrigin(origins = FRONTEND_URL)
+    @GetMapping("/threats")
+    public ResponseEntity<String> threats() {
+        return ResponseEntity.ok(ipService.getThreatsNumber());
+    }
+}
