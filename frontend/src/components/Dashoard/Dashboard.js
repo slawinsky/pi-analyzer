fd4427ae4e0a3c193a4308350018c113c5afbd7b
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-05-12 11:01:27 +0200

    implement fetching hosts & threats data from APi

--- frontend/src/components/Dashoard/Dashboard.js
+++ frontend/src/components/Dashoard/Dashboard.js
@@ -1,42 +1,70 @@
-import React from "react";
+import { React, Component } from "react";
 import { Link } from "react-router-dom";
+import { request } from "../../api/api-client";
 import "./Dashboard.css";
 
-const Dashboard = () => {
-  return (
-    <div className="dashboard content">
-      <div className="dashboard__up">
-        <div className="dashboard__threats tile tile-thin">
-          <h1 className="dashboard__title">Threats</h1>
-          <span className="dashboard__value">43</span>
-          <Link to="/home/monitoring">
-            <button className="dashboard__btn btn-green">
-              View more {">>"}
-            </button>
-          </Link>
+class Dashboard extends Component {
+  state = {
+    threats: 0,
+    hosts: 0,
+  };
+
+  async componentDidMount() {
+    await request("GET", "/threats")
+      .then((response) => {
+        this.setState({ threats: response.data });
+      })
+      .catch((error) => {
+        this.setState({ threats: "error" });
+      });
+
+    await request("GET", "/hosts/count")
+      .then((response) => {
+        this.setState({ hosts: response.data });
+      })
+      .catch((error) => {
+        this.setState({ hosts: "error" });
+      });
+  }
+
+  render() {
+    const { hosts, threats } = this.state;
+
+    return (
+      <div className="dashboard content">
+        <div className="dashboard__up">
+          <div className="dashboard__threats tile tile-thin">
+            <h1 className="dashboard__title">Threats</h1>
+            <span className="dashboard__value">{threats}</span>
+            <Link to="/home/monitoring">
+              <button className="dashboard__btn btn-green">
+                View more {">>"}
+              </button>
+            </Link>
+          </div>
+          <div className="dashboard__traffic tile tile-wide">
+            <h1 className="dashboard__title left__title">Traffic</h1>
+            <span className="dashboard__chart">chart</span>
+          </div>
         </div>
-        <div className="dashboard__traffic tile tile-wide">
-          <h1 className="dashboard__title left__title">Traffic</h1>
-          <span className="dashboard__chart">chart</span>
+        <div className="dashboard__bottom">
+          <div className="dashboard__ips tile tile-wide">
+            <h1 className="dashboard__title left__title">Common Ip's</h1>
+            <div className="dashboard__chart">chart</div>
+          </div>
+          <div className="dashboard__hosts tile tile-thin">
+            <h1 className="dashboard__title">Hosts</h1>
+            <span className="dashboard__value">{hosts}</span>
+            <Link to="/home/monitoring">
+              <button className="dashboard__btn btn-green">
+                View more {">>"}
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-      <div className="dashboard__bottom">
-        <div className="dashboard__ips tile tile-wide">
-          <h1 className="dashboard__title left__title">Common Ip's</h1>
-          <div className="dashboard__chart">chart</div>
-        </div>
-        <div className="dashboard__hosts tile tile-thin">
-          <h1 className="dashboard__title">Hosts</h1>
-          <span className="dashboard__value">8</span>
-          <Link to="/home/monitoring">
-            <button className="dashboard__btn btn-green">
-              View more {">>"}
-            </button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Dashboard;
