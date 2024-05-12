14ab4ead0bbb06352497772e9bf0ae30809d2250
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-05-12 11:02:05 +0200

    implement fetching data about connected hosts from API

--- frontend/src/components/Hosts/Hosts.js
+++ frontend/src/components/Hosts/Hosts.js
@@ -1,52 +1,65 @@
-import React from "react";
+import { React, Component } from "react";
+import { request } from "../../api/api-client";
 import "./Hosts.css";
+import Host from "../Host/Host";
 
-const Hosts = () => {
-  return (
-    <div className="content">
-      <div className="hosts tile">
-        <h1 className="hosts__title left__title">Hosts</h1>
-        <table className="hosts__table">
-          <tr>
-            <th className="hosts__heading ip">IP ADDRESS</th>
-            <th className="hosts__heading mac">MAC ADDRESS</th>
-            <th className="hosts__heading name">NAME</th>
-            <th className="hosts__heading time">CONNECTION TIME</th>
-          </tr>
-          <tr>
-            <td className="ip">192.168.33.55</td>
-            <td className="mac">00-B0-D0-63-C2-26</td>
-            <td className="name">POCO X3 PRO</td>
-            <td className="time">02:34:50</td>
-            <td>
-              <button className="hosts__btn btn-red block">block</button>
-            </td>
-          </tr>
-          <tr>
-            <td className="ip">192.168.33.55</td>
-            <td className="mac">00-B0-D0-63-C2-26</td>
-            <td className="name">POCO X3 PRO</td>
-            <td className="time">02:34:50</td>
-            <td>
-              <button className="hosts__btn btn-red block">block</button>
-            </td>
-          </tr>
-          <tr>
-            <td className="ip">192.168.33.55</td>
-            <td className="mac">00-B0-D0-63-C2-26</td>
-            <td className="name">POCO X3 PRO</td>
-            <td className="time">02:34:50</td>
-            <td>
-              <button className="hosts__btn btn-red block">block</button>
-            </td>
-          </tr>
-        </table>
-        <button className="hosts__btn btn-green view">
-          View blocked hosts {">>"}
-        </button>
+class Hosts extends Component {
+  state = {
+    data: null,
+    isLoading: true,
+  };
+
+  async componentDidMount() {
+    await request("GET", "/hosts/list")
+      .then((response) => {
+        return response.data.map((data) => JSON.parse(data));
+      })
+      .then((data) => {
+        this.setState({ data, isLoading: false });
+      })
+      .catch((error) => {
+        this.setState({ data: "error" });
+      });
+  }
+
+  render() {
+    const { data, isLoading } = this.state;
+
+    let HostList = "";
+
+    if (!isLoading) {
+      HostList = data.map((host) => (
+        <Host
+          ip={host.ip}
+          mac={host.mac}
+          name={host.name}
+          time={host.lastConnected.$date}
+        />
+      ));
+    }
+
+    return (
+      <div className="content">
+        <div className="hosts tile">
+          <h1 className="hosts__title left__title">Hosts</h1>
+          <table className="hosts__table">
+            <tbody>
+              <tr>
+                <th className="hosts__heading ip">IP ADDRESS</th>
+                <th className="hosts__heading mac">MAC ADDRESS</th>
+                <th className="hosts__heading name">NAME</th>
+                <th className="hosts__heading time">CONNECTION TIME</th>
+              </tr>
+              {HostList}
+            </tbody>
+          </table>
+          <button className="hosts__btn btn-green view">
+            View blocked hosts {">>"}
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Hosts;
