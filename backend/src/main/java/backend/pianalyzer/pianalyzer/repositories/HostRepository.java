d505c891204a0dc957cc566eee9e14bdb4bb80b8
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-05-12 10:57:46 +0200

    make small updates with hosts endpoints

--- backend/src/main/java/backend/pianalyzer/pianalyzer/repositories/HostRepository.java
+++ backend/src/main/java/backend/pianalyzer/pianalyzer/repositories/HostRepository.java
@@ -4,9 +4,11 @@ import backend.pianalyzer.pianalyzer.models.Host;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface HostRepository extends MongoRepository<Host, String> {
     Optional<Integer> countAllByIsConnectedTrue();
+    Optional<List<String>> getAllByIsConnectedTrue();
 }
