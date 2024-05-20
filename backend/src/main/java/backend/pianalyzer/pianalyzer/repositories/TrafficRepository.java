49afd68265d22207c0a00c87374605e45f761ac6
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-05-20 22:54:36 +0200

    implement /protocol endpoint

--- backend/src/main/java/backend/pianalyzer/pianalyzer/repositories/TrafficRepository.java
+++ backend/src/main/java/backend/pianalyzer/pianalyzer/repositories/TrafficRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TrafficRepository extends MongoRepository<Traffic, String> {
     @Query(value = "{'protocol':  ?0}", fields = "{'hits': 1, '_id': 0}")
     String findHitsByProtocol(String protocol);
+
+    List<Traffic> findAllBy();
 }
--- backend/src/main/java/backend/pianalyzer/pianalyzer/services/TrafficService.java
+++ backend/src/main/java/backend/pianalyzer/pianalyzer/services/TrafficService.java
@@ -1,8 +1,12 @@
 package backend.pianalyzer.pianalyzer.services;
 
+import backend.pianalyzer.pianalyzer.DTOs.responses.TrafficResponse;
 import backend.pianalyzer.pianalyzer.repositories.TrafficRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +15,14 @@ public class TrafficService {
 
     public String getHitsByProtocol(String protocol) {
         return trafficRepository.findHitsByProtocol(protocol);
+    }
+    public List<TrafficResponse> getProtocolsInfo() {
+        List<TrafficResponse> protocols = new ArrayList<>();
+        trafficRepository.findAllBy().forEach((protocol) -> {
+            var p = TrafficResponse.builder().protocol(protocol.getProtocol()).hits(protocol.getHits()).build();
+            protocols.add(p);
+        });
+
+        return protocols;
     }
 }
